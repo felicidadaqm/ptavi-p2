96b009bdd098cb5e8fd00eeadb9a0bc9d806fbc7
Felicidad <felicidadaqm@gmail.com>
2018-09-26 17:58:57 +0200

    calcplus y calcplusplus funcionan también

---
+++ calcplus.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+import calcoohija
+
+class CalculadoraPlus(calcoohija.CalculadoraHija):
+    operando1 = ""
+    operando2 = ""
+
+if __name__ == "__main__":
+
+    try:
+        fichero = sys.argv[1]
+        fich = open(fichero)
+    except (FileNotFoundError, IndexError):
+        sys.exit("Introduzca correctamente el nombre de fichero")
+        
+
+    for line in fich.readlines():
+
+        line = line.replace("\n", "")
+        elementos = line.split(",")
+        operación = elementos[0]
+
+        calc = CalculadoraPlus()
+
+        try:
+            calc.operando1 = int(elementos[1])
+            calc.operando2 = int(elementos[2])
+        except ValueError:
+            sys.exit("Coloca valores enteros en tu fichero")
+
+        if operación == "suma":
+            result = calc.plus()
+            for numeros in elementos[3:]:
+                result = result + int(numeros)
+
+        elif operación == "resta":
+            result = calc.minus()
+            for numeros in elementos[3:]:
+                result = result - int(numeros)
+
+        elif operación == "multiplica":
+            result = calc.multiply()
+            for numeros in elementos[3:]:
+               result = result * int(numeros)
+        
+        elif operación == "divide":
+            result = calc.division()
+            for numeros in elementos[3:]:
+                try:
+                    result = result / int(numeros)
+                except ZeroDivisionError:
+                    sys.exit("Division by zero is not allowed")
+
+        else:
+            sys.exit("Operación inválida")
+
+        print(result)
---
+++ calcplusplus.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+import calcoohija
+import csv
+
+class CalculadoraPlus(calcoohija.CalculadoraHija):
+    operando1 = ""
+    operando2 = ""
+
+if __name__ == "__main__":
+
+    calc = CalculadoraPlus()
+
+    fichero = sys.argv[1]
+
+    with open(fichero) as File:
+        reader = csv.reader(File)
+        for row in reader:
+
+            operacion = row[0]
+
+            try:
+                calc.operando1 = int(row[1])
+                calc.operando2 = int(row[2])
+
+            except ValueError:
+                sys.exit("El fichero está vacío")
+
+            if operacion == "suma":
+                result = calc.plus()
+                for numeros in row[3:]:
+                    result = result + int(numeros)
+
+            elif operacion == "resta":
+                result = calc.minus()
+                for numeros in row[3:]:
+                    result = result - int(numeros)
+
+            elif operacion == "multiplica":
+                result = calc.multiply()
+                for numeros in row[3:]:
+                    result = result * int(numeros)
+            
+            elif operacion == "divide":
+                result = calc.division()
+                for numeros in row[3:]:
+                    try:
+                        result = result / int(numeros)
+                    except ZeroDivisionError:
+                        sys.exit("Division by zero is not allowed")
+
+            else:
+                sys.exit("Operación inválida")
+
+            print(result)
+
+
+
